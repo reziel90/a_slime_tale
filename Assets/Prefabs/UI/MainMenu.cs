e71634f6eb199e2c1eae477517ffb15816c66ade
Maurizio <fbmauri87@gmail.com>
2021-03-13 17:12:27 +0100

    Main Menu
    @Reziel90

---
+++ Assets/Prefabs/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("New Game!");
+    }
+    public void LoadGame()
+    {
+        Debug.Log("Load Game!");
+    }
+    public void Options()
+    {
+        Debug.Log("Options!");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+    }
+}
