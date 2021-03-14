5b4875053b5028f972e4056669fb8a673f26e160
Maurizio <fbmauri87@gmail.com>
2021-03-14 21:27:44 +0100

    Improved Movement
    RayCast2d to detect ground

--- Assets/Prefabs/Character/CharacterControllerManager.cs
+++ Assets/Prefabs/Character/CharacterControllerManager.cs
@@ -4,27 +4,50 @@ using UnityEngine;
 
 public class CharacterControllerManager : MonoBehaviour
 {
-    private Rigidbody2D m_rigidBody2D;
+    private Rigidbody2D _rigidBody2D;
     private Vector3 m_Velocity = Vector3.zero;
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
-    [Range(0, 100f)] [SerializeField] private float m_Speed = 40f;
-    [Range(0, 100f)] [SerializeField] private float m_Jump = 10f;
+    [Range(0, 100f)] [SerializeField] private float m_Speed = 50f;
+    [Range(0, 100f)] [SerializeField] private float m_Jump = 50f;
+    [SerializeField] bool inAirMovement = false;
+    [SerializeField] bool enableDoubleJump = false;
+    private bool isGrounded;
+    private bool isDoubleJump;
+    private int groundMask;
 
     void Start()
     {
-        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        groundMask = LayerMask.GetMask("Ground");
     }
 
     public void Jump()
     {
-        Vector2 _forceJump = new Vector2(m_rigidBody2D.velocity.x, m_Jump*10f);
-        m_rigidBody2D.AddForce(_forceJump);
+        if (isGrounded || (enableDoubleJump && !isDoubleJump))
+        {
+            if (!isGrounded)
+            {
+                isDoubleJump = true;
+            }
+            Vector2 _forceJump = new Vector2(_rigidBody2D.velocity.x, m_Jump * 10f);
+            _rigidBody2D.AddForce(_forceJump);
+
+        }
     }
 
     public void Move(float horizontalMagnitude)
     {
-        float horizontalVelocity = horizontalMagnitude * m_Speed;
-        Vector3 targetVelocity = new Vector2(horizontalVelocity * 10f, m_rigidBody2D.velocity.y);
-        m_rigidBody2D.velocity = Vector3.SmoothDamp(m_rigidBody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        if (inAirMovement || isGrounded)
+        {
+            float horizontalVelocity = horizontalMagnitude * m_Speed * 10f;
+            Vector3 targetVelocity = new Vector2(horizontalVelocity, _rigidBody2D.velocity.y);
+            _rigidBody2D.velocity = Vector3.SmoothDamp(_rigidBody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.52f, groundMask).collider != null ? true : false;
+        isDoubleJump = isGrounded ? false : isDoubleJump;
     }
 }
